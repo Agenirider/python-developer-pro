71f26f5f378fe5d360c05e52f5460921d9e7045f
Anton <agenirider@mail.ru>
2020-12-29 07:03:38 +0400

    test in progress

--- hw_4/test.py
+++ hw_4/test.py
@@ -1,20 +1,94 @@
 import unittest
-
+import redis
+import datetime
+import functools
 import api
+import hashlib
+
+
+def is_redis_available(redis_connection):
+    try:
+        redis_connection.ping()
+        return True
+    except (redis.exceptions.ConnectionError, ConnectionRefusedError, redis.exceptions.TimeoutError):
+        return False
+    return False
+
+
+def cases(cases):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args):
+            for c in cases:
+                new_args = args + (c if isinstance(c, tuple) else (c,))
+                f(*new_args)
+
+        return wrapper
+
+    return decorator
 
 
 class TestSuite(unittest.TestCase):
     def setUp(self):
         self.context = {}
         self.headers = {}
-        self.store = None
+        self.store = redis.Redis('127.0.0.1', socket_connect_timeout=1, port=6379, db=0)
 
     def get_response(self, request):
+        print(self.store)
         return api.method_handler({"body": request, "headers": self.headers}, self.context, self.store)
 
     def test_empty_request(self):
         _, code = self.get_response({})
         self.assertEqual(api.INVALID_REQUEST, code)
+
+    def set_valid_auth(self, request):
+        if request.get("login") == api.ADMIN_LOGIN:
+            request["token"] = hashlib.sha512(
+                (datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT).encode('utf-8')).hexdigest()
+        else:
+            msg = request.get("account", "") + request.get("login", "") + api.SALT
+            request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
+
+    @cases([
+        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "", "arguments": {}},
+        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "sdd", "arguments": {}},
+        {"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}},
+    ])
+    def test_bad_auth(self, request):
+        _, code = self.get_response(request)
+        self.assertEqual(api.FORBIDDEN, code)
+
+    @cases([
+        {"phone": "79175002040", "email": "stupnikov@otus.ru"},
+        {"phone": 79175002040, "email": "stupnikov@otus.ru"},
+        {"gender": 1, "birthday": "01.01.2000", "first_name": "a", "last_name": "b"},
+        {"gender": 0, "birthday": "01.01.2000"},
+        {"gender": 2, "birthday": "01.01.2000"},
+        {"first_name": "a", "last_name": "b"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
+         "first_name": "a", "last_name": "b"},
+    ])
+    def test_ok_score_request(self, arguments):
+        request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+        self.set_valid_auth(request)
+        response, code = self.get_response(request)
+        self.assertEqual(api.OK, code, arguments)
+        score = response.get("score")
+        self.assertTrue(isinstance(score, (int, float)) and score >= 0, arguments)
+        self.assertEqual(sorted(self.context["has"]), sorted(arguments.keys()))
+
+    def test_empty_request(self):
+        _, code = self.get_response({})
+        self.assertEqual(api.INVALID_REQUEST, code)
+
+    def test_redis_available(self):
+        store = redis.Redis('127.0.0.1', socket_connect_timeout=1, port=6379, db=0)
+        self.assertTrue(is_redis_available(store))
+
+    def test_redis_not_available(self):
+        store = redis.Redis('1.1.1.1', socket_connect_timeout=1)
+        self.assertFalse(is_redis_available(store))
 
 
 if __name__ == "__main__":
